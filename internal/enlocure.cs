3108604a5939ed0490289398cb54c2e99d69aac3
rnicolas <nicolas.royer@ynov.com>
2026-02-17 15:31:50 +0100

    feat(enclosure):creation of the folder cmd and internal and the class enclosure

---
+++ internal/enlocure.cs
@@ -0,0 +1,9 @@
+// création de la class Enclosure 
+public class Enclosure 
+{ public int idEnclosure;
+  public int residentNumber;
+  public float probaSick;
+  public bool overcrowding;
+  public int purchasePriceEnclosure;
+  public int sellingPriceEnclosure;
+}
