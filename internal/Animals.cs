b2294d52b944ea21480ac88f14503d988a855fa9
rnicolas <nicolas.royer@ynov.com>
2026-02-17 16:33:09 +0100

    feat(Animals): added class for tiger,eagle and chicken

--- internal/Animals.cs
+++ internal/Animals.cs
@@ -1,7 +1,7 @@
 class Animals
 {
     public string Name;
-    public string Breed;
+    public string species;
     public string FoodType;
     public float ActualHunger;
     public int MaxHunger;
@@ -13,18 +13,54 @@ class Animals
 }
 
 
-class Tiger : Animals 
+class Tiger : Animals
 {
-    
+    public Tiger(string name, string sexe)
+    {
+        Name = name;
+        species = "Tiger";
+        FoodType = "Carnivore";
+        ActualHunger = 0f;
+        MaxHunger = 100;
+        LifeTime = 25f;
+        Sexe = sexe;
+        BuyPrice = buyPrice;
+        SellPrice = sellPrice;
+    }
 }
+
 
 class Eagle : Animals
 {
+     public Eagle(string name, string sexe)
+    {
+        Name = name;
+        species = "Eagle";
+        FoodType = "Carnivore";
+        ActualHunger = 0f;
+        MaxHunger = 100;
+        LifeTime = 25f;
+        Sexe = sexe;
+        BuyPrice = buyPrice;
+        SellPrice = sellPrice;
+    }
     
 }
 
 class Chicken : Animals
-{
+{ 
+    public Chicken(string name, string sexe)
+    {
+        Name = name;
+        species = "Chicken";
+        FoodType = "omnivorous";
+        ActualHunger = 0f;
+        MaxHunger = 100;
+        LifeTime = 15f;
+        Sexe = sexe;
+        BuyPrice = buyPrice;
+        SellPrice = sellPrice;
+    }
     
 }
 
