c86c5d0e8fc766b1fa37bd8e778dca1d811ffe47
lanathan <nathan.lamarche@ynov.com>
2026-02-17 15:30:32 +0100

    feat(Animals) : Initialize the Animals class

---
+++ internal/Animals.cs
@@ -0,0 +1,30 @@
+class Animals
+{
+    public string Name;
+    public string Breed;
+    public string FoodType;
+    public float ActualHunger;
+    public int MaxHunger;
+    public float LifeTime;
+    public string Sexe;
+    public float BuyPrice;
+    public float SellPrice;
+    
+}
+
+
+class Tiger : Animals 
+{
+    
+}
+
+class Eagle : Animals
+{
+    
+}
+
+class Chicken : Animals
+{
+    
+}
+
