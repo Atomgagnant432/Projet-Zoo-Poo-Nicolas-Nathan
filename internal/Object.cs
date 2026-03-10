20ef1015063dd1f21bc282c5a5b4426eb51084ab
lanathan <nathan.lamarche@ynov.com>
2026-03-10 16:03:12 +0100

    fix gitignore

---
+++ internal/Object.cs
@@ -0,0 +1,17 @@
+class Object
+{
+    private string _label;
+    private float _quantity;
+    private float _cost;
+    private float _sellPrice;
+
+    public Object(string label, float quantity, float cost, float sellPrice)
+    {
+        _label = label;
+        _quantity = quantity;
+        _cost = cost;
+        _sellPrice = sellPrice;
+    }
+
+
+}
