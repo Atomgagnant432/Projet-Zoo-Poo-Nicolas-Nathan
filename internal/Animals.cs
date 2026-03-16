b9022586e6d618cb70e7e6e99cfba2ca4715a3c4
lanathan <nathan.lamarche@ynov.com>
2026-03-16 17:16:09 +0100

    feat(enclosure) : finalize the encosure class

--- internal/Animals.cs
+++ internal/Animals.cs
@@ -3,6 +3,7 @@ class Animals
     public string? Name;
     public string? Species;
     public string? FoodType;
+    public float DayliFoodNeed;
     public float ActualHunger;
     public int MaxHunger;
     public float LifeTime;
@@ -10,12 +11,16 @@ class Animals
     public float Age;
     public float BuyPrice;
     public float SellPrice;
+    public float HighSaisonVisit;
+    public float DownSaisonVisit;
 
-    public Animals(string name, string species, string foodType, float actualHunger, int maxHunger, float lifeTime, string sexe, float age, float buyPrice, float sellPrice)
+
+    public Animals(string name, string species, string foodType, float dayliFoodNeed, float actualHunger, int maxHunger, float lifeTime, string sexe, float age, float buyPrice, float sellPrice, float highSaisonVisit, float downSaisonVisit)
     {
         Name = name;
         Species = species;
         FoodType = foodType;
+        DayliFoodNeed = dayliFoodNeed;
         ActualHunger = actualHunger;
         MaxHunger = maxHunger;
         LifeTime = lifeTime;
@@ -23,29 +28,32 @@ class Animals
         Age = age;
         BuyPrice = buyPrice;
         SellPrice = sellPrice;
+        HighSaisonVisit = highSaisonVisit;
+        DownSaisonVisit = downSaisonVisit;
+        
     }
 }
 
 class Tiger : Animals
 {
-    public Tiger(string name, string sexe, float buyPrice, float sellPrice)
-        : base(name, "Tiger", "Carnivore", 0f, 100, 25f, sexe, 0f, buyPrice, sellPrice)
+    public Tiger(string name, string sexe, float dayliFoodNeed, float buyPrice, float sellPrice)
+        : base(name, "Tigre", "Carnivore", dayliFoodNeed, 0f, 100, 25f, sexe, 0f, buyPrice, sellPrice, 30f, 5f)
     {
     }
 }
 
 class Eagle : Animals
 {
-    public Eagle(string name, string sexe, float buyPrice, float sellPrice)
-        : base(name, "Eagle", "Carnivore", 0f, 100, 25f, sexe, 0f, buyPrice, sellPrice)
+    public Eagle(string name, string sexe, float dayliFoodNeed, float buyPrice, float sellPrice)
+        : base(name, "Aigle", "Carnivore", dayliFoodNeed ,0f, 100, 25f, sexe, 0f, buyPrice, sellPrice, 15f, 7f)
     {
     }
 }
 
 class Chicken : Animals
 {
-    public Chicken(string name, string sexe, float buyPrice, float sellPrice)
-        : base(name, "Chicken", "Omnivore", 0f, 100, 15f, sexe, 0f, buyPrice, sellPrice)
+    public Chicken(string name, string sexe, float dayliFoodNeed, float buyPrice, float sellPrice)
+        : base(name, "Poulet", "Végétalien", dayliFoodNeed, 0f, 100, 15f, sexe, 0f, buyPrice, sellPrice, 2f, 0.5f)
     {
     }
 }
